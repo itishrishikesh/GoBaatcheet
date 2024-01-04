28e457a39fa38c3909cc59488b7e2324b94f35d5
hrishi.ubuntu@wsl <itishrishikesh@gmail.com>
2024-01-04 23:08:16 +0530

    some changes for dummy client

--- cmd/dummy_client/main.go
+++ cmd/dummy_client/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"github.com/gorilla/websocket"
+	"github.com/itishrishikesh/GoBaatcheet/models"
 )
 
 func main() {
-	// Establish Connection
+	// Establish Connection.
 	var url string
 	fmt.Println("Specify a server url or type 'local'")
 	fmt.Scan(&url)
@@ -16,24 +19,29 @@ func main() {
 	}
 	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
 	if err != nil {
-		fmt.Println("E#1Q3MKH - Unable to connect to websocket server")
+		fmt.Println("E#1Q3MKH - Unable to connect to websocket server. E:", err)
 	}
+	fmt.Println("Whatever you'll type on console will be sent to the chat server!")
 
-	// Read Confirmation String
+	// Read Confirmation String.
 	go func() {
 		for {
-			_, msg, err := conn.ReadMessage()
-			if err != nil || msg == nil {
-				fmt.Println("E#1Q3MWU - Failed to read messge from server")
+			var message models.Message
+			err := conn.ReadJSON(&message)
+			if err != nil {
+				fmt.Println("E#1Q3MWU - Failed to read messge from server. E:", err)
+				return
 			}
-			fmt.Println("From Server:", string(msg))
+			fmt.Printf("\n%s: %s\n", message.Sender, message.Msg)
 		}
 	}()
 
-	// Send whatever is being typed back in console back.
+	// Send whatever is being typed in console to everyone connected.
 	for {
-		var msg string
-		fmt.Scan(&msg)
-		conn.WriteJSON(string(msg))
+		line, _, _ := bufio.NewReader(os.Stdin).ReadLine()
+		err := conn.WriteMessage(1, line)
+		if err != nil {
+			fmt.Println("E#1Q5F07 - Failed to write message to server. E:", err)
+		}
 	}
 }
