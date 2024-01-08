94d0937248496c4a4237d2871dd266f4a07e8a64
hrishi.ubuntu@wsl <itishrishikesh@gmail.com>
2024-01-08 23:27:38 +0530

    added code components and added a parent ChatContainer component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,26 @@
 import { useState } from 'react'
-import './App.css'
 import { UsernameHolder } from './component/UsernameHolder'
+import { ChatContainer } from './component/ChatContainer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState<string>("")
+
+  const logIn = () => {
+    setUsername(prompt("enter username") ?? "")
+    const ws = new WebSocket("ws://localhost:8080/ws")
+    ws.addEventListener("open", () => ws.send(username))
+    ws.addEventListener("message", (event) => console.log(event.data))
+  }
 
   return (
     <>
+      username.length ?
       <div>
-        <h1>Chat Application</h1>
-        <UsernameHolder />
-        <ChatHolder />
-        <ChatSender />
+        <h4>Go Baatcheet</h4>
+        <UsernameHolder sender='Placeholder - Sender' receiver='Placeholder - Sender' />
+        <ChatContainer />
       </div>
+      : <div>Please <button onClick={() => logIn()}>enter</button> username!</div>
     </>
   )
 }
--- frontend/src/component/ChatSender.tsx
+++ frontend/src/component/ChatSender.tsx
@@ -1,3 +1,9 @@
-export const ChatSender = () => {
-    return <div>ChatSender!</div>
+import { useState } from "react"
+
+export const ChatSender = (props: {send: any}) => {
+    const [message, setMessage] = useState("")
+    return <div>
+        <input name="message" onChange={(e) => setMessage(e.target.value)}></input>
+        <button onClick={() => props.send(message)}>Send</button>
+    </div>
 }
