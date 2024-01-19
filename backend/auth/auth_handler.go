d832c19a9d871a76c60b1a6300e560e1ed568436
hrishi.ubuntu@wsl <itishrishikesh@gmail.com>
2024-01-19 23:34:41 +0530

    experiments with sockets

--- backend/auth/auth_handler.go
+++ backend/auth/auth_handler.go
@@ -1,38 +1,32 @@
 package auth
 
 import (
-	"log"
+	"GoBaatcheet/constants"
 	"net/http"
 
 	authorizer "github.com/authorizerdev/authorizer-go"
 )
 
+const authorizerServer = "http://localhost:8082/"       // Todo: Move this to centralized configuration server
+const clientId = "c87ad9f9-e076-429f-b175-777e73570a9b" // Todo: Move this to centralized configuration server
+
 func Authenticate(request *http.Request) bool {
 	defaultHeaders := map[string]string{}
-	client, err := authorizer.NewAuthorizerClient("c87ad9f9-e076-429f-b175-777e73570a9b", "http://localhost:8082/", "", defaultHeaders)
-
+	client, err := authorizer.NewAuthorizerClient(clientId, authorizerServer, constants.EMPTY_STR, defaultHeaders)
 	if err != nil {
 		panic(err)
 	}
 
 	token := request.URL.Query().Get("access_token")
-
-	log.Println("Access token", token)
-
 	if token == "" {
 		return false
 	}
-
 	response, err := client.ValidateJWTToken(&authorizer.ValidateJWTTokenInput{
 		TokenType: authorizer.TokenTypeIDToken,
 		Token:     token,
 	})
-
 	if err != nil {
 		panic(err)
 	}
-
-	log.Println("Validated Token", response)
-
 	return response.IsValid
 }
